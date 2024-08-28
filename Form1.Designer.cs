1934f5ecd39a2181ac1c7c6f3c7818a87384183f
Ali Esmaeel <esmaeel@mobilapp.io>
2024-08-28 13:14:41 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace SpaceSooterNew
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Timer_background = new System.Windows.Forms.Timer(this.components);
+            this.MoveLeft = new System.Windows.Forms.Timer(this.components);
+            this.MoveRight = new System.Windows.Forms.Timer(this.components);
+            this.MoveUp = new System.Windows.Forms.Timer(this.components);
+            this.MoveDown = new System.Windows.Forms.Timer(this.components);
+            this.bulletsMove = new System.Windows.Forms.Timer(this.components);
+            this.Player = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Timer_background
+            // 
+            this.Timer_background.Enabled = true;
+            this.Timer_background.Interval = 10;
+            this.Timer_background.Tick += new System.EventHandler(this.Timer_background_Tick);
+            // 
+            // MoveLeft
+            // 
+            this.MoveLeft.Interval = 5;
+            this.MoveLeft.Tick += new System.EventHandler(this.MoveLeft_Tick);
+            // 
+            // MoveRight
+            // 
+            this.MoveRight.Interval = 5;
+            this.MoveRight.Tick += new System.EventHandler(this.MoveRight_Tick);
+            // 
+            // MoveUp
+            // 
+            this.MoveUp.Interval = 5;
+            this.MoveUp.Tick += new System.EventHandler(this.MoveTop_Tick);
+            // 
+            // MoveDown
+            // 
+            this.MoveDown.Interval = 5;
+            this.MoveDown.Tick += new System.EventHandler(this.MoveDown_Tick);
+            // 
+            // bulletsMove
+            // 
+            this.bulletsMove.Interval = 1;
+            this.bulletsMove.Tick += new System.EventHandler(this.bulletsMove_Tick);
+            // 
+            // Player
+            // 
+            this.Player.BackgroundImage = global::SpaceSooterNew.Properties.Resources.player;
+            this.Player.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.Player.Location = new System.Drawing.Point(224, 610);
+            this.Player.Name = "Player";
+            this.Player.Size = new System.Drawing.Size(82, 58);
+            this.Player.TabIndex = 0;
+            this.Player.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(582, 680);
+            this.Controls.Add(this.Player);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(600, 727);
+            this.MinimumSize = new System.Drawing.Size(600, 727);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer Timer_background;
+        private System.Windows.Forms.PictureBox Player;
+        private System.Windows.Forms.Timer MoveLeft;
+        private System.Windows.Forms.Timer MoveRight;
+        private System.Windows.Forms.Timer MoveUp;
+        private System.Windows.Forms.Timer MoveDown;
+        private System.Windows.Forms.Timer bulletsMove;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,186 @@
+﻿using SpaceSooterNew.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpaceSooterNew
+{
+    public partial class Form1 : Form
+    {
+        PictureBox[] stars;
+        int starsSpeed;
+        int bounds;
+        Random random;
+        int PlayerSpeed;
+        PictureBox[] bullets; 
+        int bulletsSpeed;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            starsSpeed = 4;
+            PlayerSpeed = 4;
+            random = new Random();
+            stars = new PictureBox[20];
+            bulletsSpeed = 15;
+            bullets = new PictureBox[3];
+
+            PrintStars();
+            PrintBullets();
+        }
+      
+        private void PrintBullets()
+        {
+
+            for (int i = 0; i < bullets.Length; i++)
+            {
+
+                bullets[i] = new PictureBox();
+                bullets[i].BackColor = Color.Yellow;
+                bullets[i].Size = new Size(3, 5);
+                bullets[i].BorderStyle = BorderStyle.None;
+                this.Controls.Add(bullets[i]);
+            }
+        }
+
+        private void PrintStars()
+        {
+            for (int i = 0; i < stars.Length; i++)
+            {
+                stars[i] = new PictureBox();
+                stars[i].BorderStyle = BorderStyle.None;
+                stars[i].Location = new Point(random.Next(0, this.Width), random.Next(0, this.Height));
+                stars[i].Size = new Size(2, 2);
+                stars[i].BackColor = Color.White;
+                this.Controls.Add(stars[i]);
+            }
+        }
+
+        private void Timer_background_Tick(object sender, EventArgs e)
+        {
+        
+            //// timer will move stars down
+            for (int i = 0; i < stars.Length; i++)
+            {
+                stars[i].Top += starsSpeed;
+
+                if (stars[i].Top > this.Height)
+                {
+                    stars[i].Top = 0;
+                }
+            }
+        }
+
+        private void MoveLeft_Tick(object sender, EventArgs e)
+        {
+            if(Player.Left > 10)
+            {
+                Player.Left -= PlayerSpeed;
+            }
+        }
+
+        private void MoveRight_Tick(object sender, EventArgs e)
+        {
+            if(Player.Right < this.Width - 25)
+            {
+                Player.Left += PlayerSpeed;
+            }
+        }
+
+        private void MoveTop_Tick(object sender, EventArgs e)
+        {
+            if (Player.Top > 10)
+            {
+                Player.Top -= PlayerSpeed;
+            }
+        }
+
+        private void MoveDown_Tick(object sender, EventArgs e)
+        {
+            if (Player.Top < this.Height -90)
+            {
+                Player.Top += PlayerSpeed;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            StopAllTimers();
+       
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Enter:
+                    bulletsMove.Enabled=true;
+                    break;
+                case Keys.Left:
+                    MoveLeft.Start();
+                    break;
+                case Keys.Right:
+                    MoveRight.Start();
+                    break;
+                case Keys.Up:
+                    MoveUp.Start();
+                    break;
+                case Keys.Down:
+                    MoveDown.Start();
+                    break;
+            }
+        }
+        private void StopAllTimers()
+        {
+            MoveDown.Stop();
+            MoveLeft.Stop();
+            MoveRight.Stop();
+            MoveUp.Stop();
+        }
+
+        private void bulletsMove_Tick(object sender, EventArgs e)
+        {
+ 
+            int lastBullet = bullets.Length - 1;
+
+            for (int i = 0; i < bullets.Length; i++) {
+
+              
+
+
+                if (bullets[lastBullet].Top > 0) {
+
+                    bullets[i].Visible = true;
+                    bullets[i].Top -= bulletsSpeed;
+                }
+                else
+                {
+                    if (bullets[lastBullet].Top < 0)
+                    {
+                        bulletsMove.Enabled = false;
+                        bulletsMove.Stop();
+                        break;
+
+                    }
+
+                    bullets[i].Visible = false;
+                    bullets[i].Location = new Point(Player.Location.X +30, Player.Location.Y - i*30);
+                }
+                
+               
+
+
+            }
+        }
+
+    }
+}
